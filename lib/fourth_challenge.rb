809e53f80170bb49b7d7c11e5377449452a28dd3
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-06 23:52:55 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,7 +4,8 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [{
+      :hero_friends => [
+        {
         :name => "Benvolio",
         :age => "17",
         :attitude => "worried"
@@ -21,7 +22,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {
+        :name => ""
+        :age 
+        :attitude
+        }
       ]
    }
 }
